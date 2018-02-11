dd6c3aace60397cbe47d0777d3b2b73eed0c22dd
Shubham Choudhury <escortnotice@gmail.com>
2018-02-11 19:05:49 +0530

    basic spring boot app hosted on docker

---
+++ src/main/java/com/example/demo/BasicSpringBootAndDockerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootAndDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootAndDockerApplication.class, args);
+	}
+}
