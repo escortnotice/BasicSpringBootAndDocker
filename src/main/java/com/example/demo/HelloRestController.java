ce2080c34f859b80e9edcbe11c6926cf33f6fc0d
escortnotice <escortnotice@gmail.com>
2019-12-19 18:38:01 +0530

    Update HelloRestController.java

--- src/main/java/com/example/demo/HelloRestController.java
+++ src/main/java/com/example/demo/HelloRestController.java
@@ -19,7 +19,7 @@ public class HelloRestController {
 	@GetMapping
 	public String hello() {
 		log.info("Rest Service Called: serverdatetime {}",Calendar.getInstance().getTime());
-		return "Hola!! From Docker";
+		return "Holass!! From Docker";
 	}
 	
 }
